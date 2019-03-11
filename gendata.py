488ef1fa0ca14a411f4cfc6353f3a2b68ec457b1
Sandra Derbring <sandra.derbring@gmail.com>
2019-03-11 23:31:59 +0100

    added code for preparing the data

--- gendata.py
+++ gendata.py
@@ -9,31 +9,219 @@ import pandas as pd
 
 # add whatever additional imports you may need here. You may not use the
 # scikit-learn OneHotEncoder, or any related automatic one-hot encoders.
+import random
+import nltk
+from nltk import ngrams
 
-parser = argparse.ArgumentParser(description="Convert text to features")
-parser.add_argument("-N", "--ngram", metavar="N", dest="ngram", type=int, default=3, help="The length of ngram to be considered (default 3).")
-parser.add_argument("-S", "--start", metavar="S", dest="startline", type=int,
-                    default=0,
-                    help="What line of the input data file to start from. Default is 0, the first line.")
-parser.add_argument("-E", "--end", metavar="E", dest="endline",
-                    type=int, default=None,
-                    help="What line of the input data file to end on. Default is None, whatever the last line is.")
-parser.add_argument("inputfile", type=str,
-                    help="The file name containing the text data.")
-parser.add_argument("outputfile", type=str,
-                    help="The name of the output file for the feature table.")
 
-args = parser.parse_args()
+def load(inputfile):
 
-print("Loading data from file {}.".format(args.inputfile))
-print("Starting from line {}.".format(args.startline))
-if args.endline:
-    print("Ending at line {}.".format(args.endline))
-else:
-    print("Ending at last line of file.")
+    # Loads the file and returns the lines
+    with open(inputfile, 'r') as myFile:
+        lines = myFile.readlines()
 
-print("Constructing {}-gram model.".format(args.ngram))
-print("Writing table to {}.".format(args.outputfile))
+    return lines
+
+
+def prepare_data(lines, startline, endline, testnumber):
+
+    corpus_lines = []
+
+    print("This file has {} lines.".format(len(lines)))
     
-# THERE ARE SOME CORNER CASES YOU HAVE TO DEAL WITH GIVEN THE INPUT
-# PARAMETERS BY ANALYZING THE POSSIBLE ERROR CONDITIONS.
+    print("Starting from line {}.".format(startline))
+    if endline:
+        print("Ending at line {}.".format(endline))
+    else:
+        print("Ending at last line of file.")    
+
+
+    #Fetch all lines between the given start and end lines
+    for line in lines[startline:endline]:
+        corpus_lines.append(line)
+    
+    #Shuffles the list of lines and split them
+    #into training and test sets
+    random.shuffle(corpus_lines)
+    test_lines = corpus_lines[:testnumber]
+    training_lines = corpus_lines[testnumber:]
+            
+    return corpus_lines, training_lines, test_lines       
+
+
+def lines_to_words(lines):
+
+    words = []
+
+    #Using NLTK to tokenize the sentence and strip the tags
+    for line in lines:
+        for pair in line.split():
+            word, tag = nltk.tag.str2tuple(pair)
+            words.append(word)
+                    
+    return words
+
+
+def create_vocabulary(words):
+
+    vocabulary = {}
+    count = 1
+
+    #Adding start item at index 0 in vocabulary
+    vocabulary['<start>'] = 0
+
+    #Adding words in set from index 1
+    for word in words:
+        if word not in vocabulary:
+            vocabulary[word] = count
+            count+=1
+
+    return vocabulary
+ 
+
+def one_hot_transform(vocabulary):
+
+    onehots = {}
+    
+    for word in vocabulary:
+        #Creates an array of zeros in the lenght of V
+        onehot = np.zeros((len(vocabulary),), dtype='int')
+
+        #Assign 1 on the index where the word belongs
+        windex = vocabulary[word]
+        onehot[windex] = 1        
+
+        onehots[word] = onehot
+
+    return onehots
+
+
+def produce_features(ngrams, onehots):
+
+    vectors = []
+
+    for gram in ngrams:
+
+        #Get one hot vectors for all word but the last
+        label = gram[-1]
+        ohv_list = []
+        for g in gram[:-1]:
+            ohv_list.append(onehots[g])
+                
+        ohv_array = np.concatenate(ohv_list)
+
+        #Append label at the end of the array
+        features = np.append(ohv_array, label)
+
+        vectors.append(features)
+
+    #Add all features in one stack
+    all_arrays = np.stack(vectors)
+    
+    return all_arrays
+
+
+def print_feature_table(outputfile, arrays, ngrams, datatype):
+    
+    #Name file after type of data (train or test)
+    filename = outputfile.split('.')[0]
+    outputfile = filename+'.'+datatype
+
+    #Makes a panda dataframe                                                                                                                                                    
+    pdframe = pd.DataFrame(arrays)
+    
+    #Prints to file                                                                                                                                                           
+    with open(outputfile, 'w') as out:
+        pdframe.to_csv(out, encoding="utf-8", columns=None)
+    
+
+def parse_arguments(parser):
+
+    """Parses the arguments from the command line.                                                                                                                                  
+    Args:                                                                                                                                                                           
+    parser:   An ArgumentParser object.                                                                                                                                           
+    Returns:                                                                                                                                                                        
+        A Namespace object built from the parsed attributes.                                                                                                                        
+    """ 
+ 
+    #Adds arguments from the command line to the parser
+    parser.add_argument("inputfile", type=str, help="The file name containing the text data.")
+    parser.add_argument("-N", "--ngram", metavar="N", dest="ngram", type=int, default=3, help="The length of ngram to be considered (default 3).")
+    parser.add_argument("-S", "--start", metavar="S", dest="startline", type=int, default=0, help="What line of the input data file to start from. Default is 0, the first line.")
+    parser.add_argument("-E", "--end", metavar="E", dest="endline", type=int, default=None, help="What line of the input data file to end on. Default is None, the last line.")
+    parser.add_argument("outputfile", type=str, help="The name of the output file for the feature table.")
+    parser.add_argument("-T", "--lines", metavar="T", dest="testlines", type=int, default=None, help="The number of lines assigned to test data (default None).")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+
+    ngram_cond = True
+    startline_cond = True
+    endline_cond = True
+    testline_cond = True
+    
+    #Parses the arguments from the command line                                                                                                                                     
+    args = parse_arguments(argparse.ArgumentParser(description="Convert text to features."))
+
+    #Read the file into lines
+    print("Loading data from file {}.".format(args.inputfile))
+    lines = load(args.inputfile)
+
+    #Checking conditions
+    if args.ngram < 2:
+        ngram_cond = False
+        print("The ngram needs to be at least 2. Try again. ")
+        
+    if args.startline > len(lines) or args.startline > args.endline:
+        startline_cond = False
+        print("The files have too few lines or your starting point occurs later than your end point. Try again.")
+
+    if args.endline:
+        if args.endline > len(lines):
+            endline_cond = False
+            print("The files have too few lines to end at this line. Try again.")
+
+    if args.testlines:
+        if args.testlines > (args.endline-args.startline):
+            testline_cond = False
+            print("Your test set can't be larger than the total set")
+
+        
+    #If all conditions hold, run the rest of the script
+    if ngram_cond and startline_cond and endline_cond and testline_cond:
+
+        #Splitting data into sets
+        corpus_lines, training_lines, test_lines = prepare_data(lines, args.startline, args.endline, args.testlines)
+
+        #Tokenize each set
+        corpus_words = lines_to_words(corpus_lines)
+        training_words = lines_to_words(training_lines)
+        test_words = lines_to_words(test_lines)
+
+        #Creating ngrams for training and test sets with a start symbol on the far right
+        training_ngrams = list(ngrams(training_words, args.ngram, pad_left=True, left_pad_symbol='<start>'))
+        test_ngrams = list(ngrams(test_words, args.ngram, pad_left=True, left_pad_symbol='<start>'))
+
+        #Create vocabulary and one hot vectors for the corpus
+        vocabulary = create_vocabulary(corpus_words)
+        onehots = one_hot_transform(vocabulary)
+
+        #Produce features for sets
+        print("Constructing {}-gram model for training data with {} words".format(args.ngram, len(training_words)))
+        training_data = produce_features(training_ngrams, onehots)
+        
+        print("Constructing {}-gram model for test data with {} words".format(args.ngram, len(test_words)))
+        test_data = produce_features(test_ngrams, onehots)
+
+        #Write sets to file
+        print("Writing table to training file")
+        print_feature_table(args.outputfile, training_data, training_ngrams, "train")
+
+        print("Writing table to test file")
+        print_feature_table(args.outputfile, test_data, test_ngrams,  "test")
+
+
+        # THERE ARE SOME CORNER CASES YOU HAVE TO DEAL WITH GIVEN THE INPUT
+        # PARAMETERS BY ANALYZING THE POSSIBLE ERROR CONDITIONS.
