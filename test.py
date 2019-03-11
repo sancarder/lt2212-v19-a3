45490827b57e11460439cee51c6caeca0b95fe9a
Sandra Derbring <sandra.derbring@gmail.com>
2019-03-11 23:35:00 +0100

    added code for testing the model

--- test.py
+++ test.py
@@ -10,19 +10,96 @@ from sklearn.linear_model import LogisticRegression
 
 # add whatever additional imports you may need here.
 
-parser = argparse.ArgumentParser(description="Test a maximum entropy model.")
-parser.add_argument("-N", "--ngram", metavar="N", dest="ngram", type=int, default=3, help="The length of ngram to be considered (default 3).")
-parser.add_argument("datafile", type=str,
-                    help="The file name containing the features in the test data.")
-parser.add_argument("modelfile", type=str,
-                    help="The name of the saved model file.")
+def load_data(datafile):
 
-args = parser.parse_args()
+    #Load the data and divide it into features and labels                                                                                                                            
+    #Read the data and transform it into an numpy array                                                                                                                             
+    df = pd.read_csv(datafile)
 
-print("Loading data from file {}.".format(args.datafile))
-print("Loading model from file {}.".format(args.modelfile))
+    #Fetch data from all columns but the last one (labels)                                                                                            
+    features = df.iloc[:, :-1]                                                                                                                                                     
+    
+    #Fetch data from the last column to get class labels                                                                                                                     
+    labels = df.iloc[:, -1]
+    
+    return features, labels
 
-print("Testing {}-gram model.".format(args.ngram))
 
-print("Accuracy is ...")
-print("Perplexity is...")
+def test_model(model, features, labels):
+    
+    #Sets the classes_ attribute to the known labels
+    setattr(model, 'classes_', labels)
+
+    #Predict labels
+    predicted_classes = model.predict(features)    
+    for i in range(len(features)):
+        #print("N-gram: {}, Predicted: {}, Real: {}".format(features.values[i], predicted_classes.values[i], labels[i]))
+
+    #Get predicted log probabilities
+    predicted_logs = model.predict_log_proba(features)
+    
+    #Get predicted probabilities
+    predicted_probas = model.predict_proba(features)
+
+    #Calculate entropy and perplexity on the probability distribution
+    entropy = calculate_entropy(predicted_probas, predicted_logs)
+    perplexity = calculate_perplexity(entropy)
+    
+    #Calculate accuracy
+    accuracy = model.score(features, labels)
+        
+    return accuracy, perplexity
+
+
+def calculate_entropy(probs, logs):
+
+    #Calculates the entropy of a probability distribution
+    #The probability distribution is all probabilities for all ngrams
+        
+    values = []
+
+    #Multiply each probability measure with its log probability
+    for i in range(len(probs)):
+        for j in range(len(probs[i])):
+            values.append(probs[i][j] * logs[i][j]) #p * l
+
+    #Sum the calculations
+    entropy = -sum(values)
+    
+    return entropy
+
+
+def calculate_perplexity(entropy):
+
+    #Calculate perplexity by raising the power of the entropy
+    perplexity = np.power(2, entropy)
+
+    return perplexity
+
+
+def parse_arguments(parser):
+
+    #Adds arguments from the command line to the parser                                                                                                                              
+    parser.add_argument("-N", "--ngram", metavar="N", dest="ngram", type=int, default=3, help="The length of ngram to be considered (default 3).")
+    parser.add_argument("datafile", type=str, help="The file name containing the features in the test data.")
+    parser.add_argument("modelfile", type=str, help="The name of the saved model file.")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+
+    #Parses the arguments from the command line                                                                                                                                     
+    args = parse_arguments(argparse.ArgumentParser(description="Test a maximum entropy model."))
+
+    print("Loading data from file {}.".format(args.datafile))
+    features, labels = load_data(args.datafile)
+    
+    print("Loading model from file {}.".format(args.modelfile))
+    with open(args.modelfile, 'rb') as modelfile:
+        model = pickle.load(modelfile)
+
+    #print("Testing {}-gram model.".format(args.ngram))                                                                                                                         
+    accuracy, perplexity = test_model(model, features, labels)
+
+    print("Accuracy is: ".format(accuracy))                                                                                                                                             print("Perplexity is: ".format(perplexity)) 
